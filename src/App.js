fed1935f051722f91644357c4bc3407c6dedc73f
Manoj-36 <n1k0l4t35l4369@gmail.com>
2021-09-03 18:09:54 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./Style.css";
 import Main from "./Main";
+import Nav from "./Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Nav />
       <Router>
         <Switch>
           <Route exact path="/">
