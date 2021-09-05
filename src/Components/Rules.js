551cadb72722d584cab9b263e3672ad27526eb27
Manoj-36 <n1k0l4t35l4369@gmail.com>
2021-09-05 20:10:00 +0530

    updates

--- src/Components/Rules.js
+++ src/Components/Rules.js
@@ -3,20 +3,15 @@ import React from "react";
 export default function Rules() {
   return (
     <div className="Rules">
-      <div class="message">
-  &check; Made by <a href="https://codepen.io/pieter-biesemans/" target="_blank" title="&check; check out my other work">Pieter Biesemans</a> with &#9749;, &#128420; &amp; &#8986;</i>
-</div>
-
-<div class="card" data-tilt data-tilt-scale="0.95" data-tilt-startY="40">
-  <div class="welcome">
-    Welcome to
-  </div>
-  <div class="year">
-    <span>Twenty</span>
-    <span>Twenty</span>
-    <span>One</span>
-  </div>  
-</div>
+      <h2 className="R-head">Rules</h2>
+      <div class="R-grid-container">
+        <div className="R-do">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+        <div className="R-dont">2</div>
+      </div>
     </div>
   );
 }
