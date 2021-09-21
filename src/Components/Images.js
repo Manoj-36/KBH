6f2a8309019ec262cb972c1a307bcfa7bf2d56ac
Manoj-36 <n1k0l4t35l4369@gmail.com>
2021-09-21 19:31:14 +0530

    updates

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Images.css";
+import Kh from "../Components/Hostels/img/kh.jpeg";
+import Gpic from "./Hostels/img/gpic.jpeg";
+import Hostelday from "./Hostels/img/hostelday.jpeg";
+import G3 from "./Hostels/img/g3.mp4";
 
 export default function Images() {
   return (
@@ -15,10 +19,18 @@ export default function Images() {
         <a href="#slide-5">5</a> */}
 
         <div class="slides">
-          <div id="slide-1">1</div>
-          <div id="slide-2">2</div>
-          <div id="slide-3">3</div>
-          <div id="slide-4">4</div>
+          <div id="slide-1">
+            <img className="Images-slid-img" src={Kh} />
+          </div>
+          <div id="slide-2">
+            <img className="Images-slid-img" src={Gpic} />
+          </div>
+          <div id="slide-3">
+            <img className="Images-slid-img" src={Hostelday} />
+          </div>
+          <div id="slide-4">
+            <img src={G3} />
+          </div>
           <div id="slide-5">5</div>
         </div>
       </div>
